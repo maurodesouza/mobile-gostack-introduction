61ff9bdc9ebbf6f9d8842e81a2e34401247e9456
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-02-20 19:00:04 -0300

    fix multiple reactotron connections

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,11 +1,13 @@
 import Reactotron from 'reactotron-react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
   const tron = Reactotron.configure()
     .useReactNative()
+    .setAsyncStorageHandler(AsyncStorage)
     .connect();
 
   console.tron = tron;
 
-  Reactotron.clear();
+  tron.clear();
 }
