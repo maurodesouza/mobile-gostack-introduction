450cc41ac9135b941aaf7b83d4c0a5d16bfa5fa2
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-02-20 13:35:08 -0300

    class 06 - styled components

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function User() {
   return (
-    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+    <Container>
       <Text> Essa é ... hmm a pagina principal ! </Text>
-    </View>
+    </Container>
   );
 }
